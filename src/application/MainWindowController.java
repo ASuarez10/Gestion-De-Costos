dbc43c716924cfd5b2fb7081671a0f0a50c3f61f
Alejandro Suarez <asuarez100501@gmail.com>
2020-04-17 21:24:38 -0500

    Interfaz

---
+++ src/application/MainWindowController.java
@@ -0,0 +1,88 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainWindowController {
+	
+    @FXML
+	void seeProducts(ActionEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("ProductWindow.fxml"));
+			Parent root = (Parent) loader.load();
+			root.getStylesheets().add("application.css");// CSS
+			ProductWindowController nc = loader.getController();
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(root));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+    
+    @FXML
+    void seeUtility(ActionEvent event) {
+    	try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("UtilityWindow.fxml"));
+			Parent root = (Parent) loader.load();
+			root.getStylesheets().add("application.css");// CSS
+			UtilityWindowController nc = loader.getController();
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(root));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+    }
+    
+    @FXML
+    void seeGraphics(ActionEvent event) {
+    	try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("GraphicsWindow.fxml"));
+			Parent root = (Parent) loader.load();
+			root.getStylesheets().add("application.css");// CSS
+			GraphicsWindowController nc = loader.getController();
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(root));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+    }
+    
+    @FXML
+    void seeIndicator(ActionEvent event) {
+    	try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("IndicatorWindow.fxml"));
+			Parent root = (Parent) loader.load();
+			root.getStylesheets().add("application.css");// CSS
+			IndicatorWindowController nc = loader.getController();
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(root));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+    }
+    
+    @FXML
+    void seeApalancamiento(ActionEvent event) {
+    	try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("ApalancamientoWindow.fxml"));
+			Parent root = (Parent) loader.load();
+			root.getStylesheets().add("application.css");// CSS
+			ApalancamientoWindowController nc = loader.getController();
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(root));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+    }
+}//final
---
+++ src/application/UtilityWindowController.java
@@ -0,0 +1,31 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class UtilityWindowController {
+
+	@FXML
+	private TextField Utility;
+	
+	@FXML
+	void backToMenu(ActionEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+			Parent root = (Parent) loader.load();
+			root.getStylesheets().add("application.css");// CSS
+			MainWindowController nc = loader.getController();
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(root));
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}//final
